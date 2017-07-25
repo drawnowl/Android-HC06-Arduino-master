405d6f2a2aa206c90c92c824c79ca42127157eff
Eugene <drawnowl@gmail.com>
2017-07-25 10:34:21 +0300

    // coordinates comparison

--- app/src/main/java/com/hc_06/yj/bluetoothapplication/ui/activities/userCabinet/LoginActivity.java
+++ app/src/main/java/com/hc_06/yj/bluetoothapplication/ui/activities/userCabinet/LoginActivity.java
@@ -95,6 +95,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     }
                 });
     }
+
     @Override
     public void onBackPressed() {
         startActivity(new Intent(LoginActivity.this, DevicesListActivity.class));
